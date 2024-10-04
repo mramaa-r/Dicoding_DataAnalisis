7abe16717d2889af42bab7c745ff9a89379d7a98
mramaa-r <93069073+mramaa-r@users.noreply.github.com>
2024-10-04 12:55:29 +0700

    Update Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import path
 
 # Set seaborn style for better visualization
 sns.set(style="whitegrid")
